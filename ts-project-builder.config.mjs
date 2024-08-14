fcbd58131119e656ca9b6393500774702bda4959
kiki-kanri <a470666@gmail.com>
2024-08-14 23:40:49 +0800

    chore: add required dependencies

--- ts-project-builder.config.mjs
+++ ts-project-builder.config.mjs
@@ -2,5 +2,6 @@ import del from 'rollup-plugin-delete';
 import { defineConfig } from 'ts-project-builder';
 
 export default defineConfig({
-	additionalInputPlugins: { afterBuiltIns: [del({ hook: 'writeBundle', targets: './dist/types/**/*.mjs' })] }
+	additionalInputPlugins: { afterBuiltIns: [del({ hook: 'writeBundle', targets: './dist/types/**/*.mjs' })] },
+	builtInInputPluginOptions: { nodeExternal: { include: ['mongoose'] } }
 });
