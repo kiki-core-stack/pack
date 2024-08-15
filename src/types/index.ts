d055dad76ed23f56d123df41611d77a5ff268746
kiki-kanri <a470666@gmail.com>
2024-08-15 13:10:33 +0800

    feat: add two-factor authentication types

--- src/types/index.ts
+++ src/types/index.ts
@@ -4,4 +4,6 @@ export type {} from '@kikiutils/types';
 
 declare global {
 	type AjvValidatorJSONSchema<T, O extends keyof T | 'id' = 'id'> = JSONSchemaType<OmitMongooseTimestampAndOtherFields<T, O | 'id'>>;
+	type TwoFactorAuthenticationMethod = 'emailOtp' | 'totp';
+	type TwoFactorAuthenticationStatus = Record<TwoFactorAuthenticationMethod, boolean>;
 }
