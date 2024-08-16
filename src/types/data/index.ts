d79c5bbe8b0dfe74f8fabb161204324653ad9e2f
kiki-kanri <a470666@gmail.com>
2024-08-16 09:37:48 +0800

    feat: add two-factor authentication types

--- src/types/data/index.ts
+++ src/types/data/index.ts
@@ -1,6 +1,8 @@
 export type {} from '@kikiutils/mongoose/types/data';
 
 declare global {
+	type TwoFactorAuthenticationCodesData = Partial<Record<TwoFactorAuthenticationMethod, string>>;
+
 	interface ApiResponseData<D extends object = {}> {
 		data?: D;
 		message?: string;
--- src/types/index.ts
+++ src/types/index.ts
@@ -3,7 +3,7 @@ import type { JSONSchemaType } from 'ajv';
 export type {} from '@kikiutils/types';
 
 declare global {
-	type AjvValidatorJSONSchema<T, O extends keyof T | 'id' = 'id'> = JSONSchemaType<OmitMongooseTimestampAndOtherFields<T, O | 'id'>>;
+	type AjvValidatorJSONSchema<T, O extends keyof T | 'id' = 'id'> = JSONSchemaType<OmitMongooseTimestampAndOtherFields<T, O | 'id' | TwoFactorAuthenticationMethod>>;
 	type TwoFactorAuthenticationMethod = 'emailOtp' | 'totp';
 	type TwoFactorAuthenticationStatus = Record<TwoFactorAuthenticationMethod, boolean>;
 }
