5e964e3e2ce6cabbe3790e48be2d488943b01099
kiki-kanri <a470666@gmail.com>
2024-08-29 02:15:59 +0800

    feat: export all constants from `constants/index.ts` in `nitropack/utils/index.ts`

--- src/nitropack/utils/index.ts
+++ src/nitropack/utils/index.ts
@@ -1,5 +1,6 @@
 export { getEnumNumberValues, getEnumStringValues } from '@kikiutils/node/enum';
 
+export * from '../../constants';
 export { commonAjvValidatorSchemas } from '../../constants/validator';
 export * from './lodash-es';
 export * from './response';
