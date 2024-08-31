4099791683de34ac02dc4eacf42d0c3757de49d9
kiki-kanri <a470666@gmail.com>
2024-08-31 22:50:52 +0800

    feat: add script for creating admin

---
+++ scripts/create-admin.ts
@@ -0,0 +1,16 @@
+import logger from '@kikiutils/node/consola';
+import { cryptoSha3256, cryptoSha3512 } from '@kikiutils/node/crypto-hash';
+
+import { AdminModel } from '../src/models';
+
+(async () => {
+	const admin = await AdminModel.create({
+		account: 'account',
+		enabled: true,
+		name: 'name',
+		password: cryptoSha3256(cryptoSha3512('password'))
+	});
+
+	logger.success(`Successfully created admin account: '${admin.account}'`);
+	process.exit();
+})();
