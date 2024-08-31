74aab91b8ab09e5e830caa5e427d693635faf8e1
kiki-kanri <a470666@gmail.com>
2024-09-01 03:03:37 +0800

    refactor: modify `createApiError` to instantiate `ApiError` instead of `H3Error`

--- src/nitropack/utils/response.ts
+++ src/nitropack/utils/response.ts
@@ -1,19 +1,9 @@
-import { createError } from 'h3';
-import type { H3Error } from 'h3';
+import ApiError from '../../classes/api-error';
 
-const statusCodeToMessageMap: Dict<string> = {
-	400: '資料格式錯誤或是非法操作！',
-	401: '尚未登入！',
-	403: '沒有權限！',
-	404: '找不到資料！',
-	409: '欄位值重複或是該資料使用中！'
-};
-
-export function createApiError<D extends object>(statusCode?: number, data?: D, message?: string): H3Error;
-export function createApiError<D extends object>(statusCode?: number, message?: string, data?: D): H3Error;
+export function createApiError<D extends object>(statusCode?: number, data?: D, message?: string): ApiError<D>;
+export function createApiError<D extends object>(statusCode?: number, message?: string, data?: D): ApiError<D>;
 export function createApiError(statusCode: number = 500, arg1: any, arg2?: any) {
-	if (typeof arg1 === 'string') [arg1, arg2] = [arg2, arg1];
-	return createError({ message: JSON.stringify({ data: arg1 || {}, message: arg2 || statusCodeToMessageMap[statusCode] || '系統錯誤！', success: false }), status: statusCode, statusCode });
+	return new ApiError(statusCode, arg1, arg2);
 }
 
 export function createApiErrorAndThrow<D extends object>(statusCode?: number, data?: D, message?: string): never;
