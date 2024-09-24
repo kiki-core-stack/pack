2efe7d0474033dbacbd800c16150f70dc5b7323e
kiki-kanri <a470666@gmail.com>
2024-09-24 10:27:57 +0800

    chore: update `build-and-publish` script

--- build-and-publish.sh
+++ build-and-publish.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-cd $(dirname "$(readlink -f "$0")")
+cd "$(dirname "$(readlink -f "$0")")"
 . ./.env
-pnpm run build && cp ./LICENSE ./package.json ./README.md ./dist && cd ./dist && npm publish --registry $NPM_REGISTRY
+pnpm run build && cp ./LICENSE ./package.json ./README.md ./dist && cd ./dist && npm publish --registry "$NPM_REGISTRY"
