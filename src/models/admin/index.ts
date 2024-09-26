5172403581b972e3c0a2c0b28e5029281fa97704
kiki-kanri <a470666@gmail.com>
2024-09-26 14:37:23 +0800

    refactor: update static method definition in model

--- src/models/admin/index.ts
+++ src/models/admin/index.ts
@@ -20,36 +20,31 @@ interface AdminModel extends BaseMongoosePaginateModel<Admin, AdminMethodsAndOve
 	findByAccount(account: string, projection?: Nullable<ProjectionType<Admin>>, options?: Nullable<QueryOptions<Admin>>): MongooseFindOneReturnType<Admin, AdminDocument>;
 }
 
-const adminSchema = new Schema<Admin, AdminModel, AdminMethodsAndOverrides>(
-	{
-		account: commonMongooseSchemas.string.short.trimmed.unique.required,
-		email: { ...commonMongooseSchemas.string.trimmed.nonRequired, lowercase: true },
-		enabled: commonMongooseSchemas.boolean.defaultFalse.required,
-		name: commonMongooseSchemas.string.short.trimmed.required,
-		password: {
-			...commonMongooseSchemas.string.private.trimmed.required,
-			max: 128,
-			min: 128,
-			set: (password: string) => cryptoSha3256(password)
-		},
-		totpSecret: commonMongooseSchemas.string.private.trimmed.nonRequired,
-		twoFactorAuthenticationStatus: {
-			emailOtp: commonMongooseSchemas.boolean.defaultFalse.required,
-			totp: commonMongooseSchemas.boolean.defaultFalse.required
-		}
+const adminSchema = new Schema<Admin, AdminModel, AdminMethodsAndOverrides>({
+	account: commonMongooseSchemas.string.short.trimmed.unique.required,
+	email: { ...commonMongooseSchemas.string.trimmed.nonRequired, lowercase: true },
+	enabled: commonMongooseSchemas.boolean.defaultFalse.required,
+	name: commonMongooseSchemas.string.short.trimmed.required,
+	password: {
+		...commonMongooseSchemas.string.private.trimmed.required,
+		max: 128,
+		min: 128,
+		set: (password: string) => cryptoSha3256(password)
 	},
-	{
-		statics: {
-			findByAccount(account: string, projection?: Nullable<ProjectionType<Admin>>, options?: Nullable<QueryOptions<Admin>>) {
-				return this.findOne({ account }, projection, options);
-			}
-		}
+	totpSecret: commonMongooseSchemas.string.private.trimmed.nonRequired,
+	twoFactorAuthenticationStatus: {
+		emailOtp: commonMongooseSchemas.boolean.defaultFalse.required,
+		totp: commonMongooseSchemas.boolean.defaultFalse.required
 	}
-);
+});
 
 adminSchema.index({ totpSecret: 1 }, { sparse: true, unique: true });
 adminSchema.method('verifyPassword', function (password: string) {
 	return cryptoSha3256(password) === this.password;
+});
+
+adminSchema.static('findByAccount', function (account: string, projection?: Nullable<ProjectionType<Admin>>, options?: Nullable<QueryOptions<Admin>>) {
+	return this.findOne({ account }, projection, options);
 });
 
 export const AdminModel = buildMongooseModel<Admin, AdminModel, AdminMethodsAndOverrides>('admin.admins', 'Admin', adminSchema);
