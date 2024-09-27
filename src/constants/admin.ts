cdfa49a62211c4f2fe838eea15394357aa487227
kiki-kanri <a470666@gmail.com>
2024-09-27 23:44:29 +0800

    refactor: use `Object.freeze` to freeze some constants

--- src/constants/admin.ts
+++ src/constants/admin.ts
@@ -3,7 +3,7 @@ export enum AdminLogType {
 	LoginSuccess = 1
 }
 
-export const adminLogTypeToTextMap: Record<AdminLogType, string> = {
+export const adminLogTypeToTextMap: ReadonlyRecord<AdminLogType, string> = Object.freeze({
 	[AdminLogType.LoginFailure]: '登入失敗',
 	[AdminLogType.LoginSuccess]: '登入成功'
-};
+});
