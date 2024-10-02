2f968c9cd6d3de1da0e839c704c468f2dd7b3f73
kiki-kanri <a470666@gmail.com>
2024-10-02 09:37:17 +0800

    fix: add h3 dependency to rollup node externals list

--- ts-project-builder.config.mjs
+++ ts-project-builder.config.mjs
@@ -3,5 +3,13 @@ import { defineConfig } from 'ts-project-builder';
 
 export default defineConfig({
 	additionalInputPlugins: { afterBuiltIns: [del({ hook: 'writeBundle', targets: './dist/types/**/*.mjs' })] },
-	builtInInputPluginOptions: { nodeExternal: { include: ['date-fns', 'mongoose'] } }
+	builtInInputPluginOptions: {
+		nodeExternal: {
+			include: [
+				'date-fns',
+				'h3',
+				'mongoose'
+			]
+		}
+	}
 });
