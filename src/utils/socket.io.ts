05cc13e7b9e5a858cc084427b0b7b3353cbebb03
kiki-kanri <a470666@gmail.com>
2024-10-04 13:43:00 +0800

    feat: add `createSocketIoClient` utility

---
+++ src/utils/socket.io.ts
@@ -0,0 +1,23 @@
+import logger from '@kikiutils/node/consola';
+import type { DefaultEventsMap, EventsMap } from '@socket.io/component-emitter';
+import { io } from 'socket.io-client';
+import type { ManagerOptions, Socket, SocketOptions } from 'socket.io-client';
+
+export const createSocketIoClient = <ListenEvents extends EventsMap = DefaultEventsMap, EmitEvents extends EventsMap = ListenEvents>(
+	uri: string,
+	auth?: Partial<ManagerOptions & SocketOptions>['auth'],
+	options?: Partial<ManagerOptions & SocketOptions>
+) => {
+	const socketIoClient = io(uri, {
+		auth,
+		autoConnect: false,
+		reconnectionDelayMax: 250,
+		rejectUnauthorized: false,
+		...options
+	}) as Socket<ListenEvents, EmitEvents>;
+
+	socketIoClient.on('connect', () => logger.info('socket.io已連線'));
+	socketIoClient.on('connect_error', (error) => logger.error('socket.io連線錯誤：', error));
+	socketIoClient.on('disconnect', () => logger.info('socket.io已斷線'));
+	return socketIoClient;
+};
