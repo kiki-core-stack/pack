4bef3c3bbab0b2dfa236d6adc6a6a2e858ee0f8c
kiki-kanri <a470666@gmail.com>
2024-10-07 23:05:38 +0800

    refactor: remove constants export from `nitro/utils/index.ts`

--- src/nitro/utils/index.ts
+++ src/nitro/utils/index.ts
@@ -1,7 +1,6 @@
 export { flattenToSingleValue } from '@kikiutils/node';
 export { getEnumNumberValues, getEnumStringValues } from '@kikiutils/node/enum';
 
-export * from '../../constants';
 export { ApiError } from '../classes/api-error';
 export * from './api-response';
 export * from './lodash-es';
