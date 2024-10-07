21bcc8c85e99b0151620d30fa39952602e6e6726
kiki-kanri <a470666@gmail.com>
2024-10-07 23:00:44 +0800

    feat: add image-related utilities for nitro

---
+++ src/nitro/utils/image.ts
@@ -0,0 +1,15 @@
+import type { PathLike } from '@kikiutils/classes/path';
+import type { Sharp } from 'sharp';
+
+import { isAcceptedImageFile } from '../../utils/file';
+import { convertAndSaveImageFile } from '../../utils/image';
+import { createApiErrorAndThrow } from './api-response';
+
+export const convertAndSaveImageFileOrThrowError = async (file: Blob, savePath: PathLike, format: Parameters<Sharp['toFormat']>[0] = 'webp', options?: Parameters<Sharp['toFormat']>[1]) => {
+	if (!(await convertAndSaveImageFile(file, savePath, format, options))) createApiErrorAndThrow(500);
+};
+
+export const validateImageFile = async (file: Blob, acceptGif?: boolean, ignoreFileSize?: boolean) => {
+	if (!(await isAcceptedImageFile(file, acceptGif))) return createApiErrorAndThrow(400);
+	if (!ignoreFileSize && file.size > 1048576 * 16) createApiErrorAndThrow(413); // Max 16MB
+};
