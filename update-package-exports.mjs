56a05fb75325a852b7037df4533b3b2787dbec30
kiki-kanri <a470666@gmail.com>
2024-10-08 13:25:42 +0800

    chore: auto-generate `exports` configuration in package.json

---
+++ update-package-exports.mjs
@@ -0,0 +1,23 @@
+import { readJson, writeJson } from '@kikiutils/fs-extra';
+import { glob } from 'glob';
+import { fromPairs, merge, sortBy, toPairs } from 'lodash-es';
+import { dirname } from 'path';
+
+(async () => {
+	const exports = { './*': { import: './*.mjs', types: './*.d.ts' } };
+	const filePaths = await glob(['**/index.d.ts', '**/index.mjs'], { cwd: './dist' });
+	filePaths.forEach((filePath) => {
+		const dirPath = dirname(filePath);
+		const isDts = filePath.endsWith('index.d.ts');
+		merge(exports, {
+			[`./${dirPath}`]: {
+				import: !isDts ? `./${filePath}` : undefined,
+				types: isDts ? `./${dirPath}/index.d.ts` : undefined
+			}
+		});
+	});
+
+	const packageJson = await readJson('./package.json');
+	packageJson.exports = fromPairs(sortBy(toPairs(exports), [0]));
+	await writeJson('./package.json', packageJson, { spaces: 2 });
+})();
