92446e4de3c7a7f5080031c80520afda06b83e43
kiki-kanri <a470666@gmail.com>
2024-10-10 20:10:41 +0800

    feat(hono-backend): add `getClientIpFromXForwardedForHeader` global util

--- src/hono-backend/globals/index.ts
+++ src/hono-backend/globals/index.ts
@@ -5,6 +5,14 @@ import './classes/api-error';
 
 declare global {
 	function defineRouteHandler(handler: (ctx: Context) => Promise<Response> | Response): (ctx: Context) => Promise<Response> | Response;
+	function getClientIpFromXForwardedForHeader(ctx: Context): string | undefined;
 }
 
 globalThis.defineRouteHandler = (handler) => handler;
+globalThis.getClientIpFromXForwardedForHeader = (ctx: Context) => {
+	const xForwardedFor = ctx.req.header('x-forwarded-for');
+	if (!xForwardedFor) return;
+	const firstCommaIndex = xForwardedFor.indexOf(',');
+	const clientIp = firstCommaIndex === -1 ? xForwardedFor : xForwardedFor.substring(0, firstCommaIndex);
+	return clientIp.trim();
+};
