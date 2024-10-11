23b1c1c41c5544a38ff3a0cc457a4bc80848b541
kiki-kanri <a470666@gmail.com>
2024-10-11 14:44:32 +0800

    feat(hono-backend): add zod openapi route handling in APIs setup

--- src/hono-backend/setups/apis.ts
+++ src/hono-backend/setups/apis.ts
@@ -16,17 +16,24 @@ const allowedMethods = [
 ] as const;
 
 let apisCount = 0;
+let createZodOpenApiRoute;
 const startAt = performance.now();
 const matchApiFilePathRegex = new RegExp(`^src/apis(.*?)(/index)?\\.(${allowedMethods.join('|')})\\.ts$`);
 const apiFilePaths = await glob(`src/apis/**/*.{${allowedMethods.join(',')}}.ts`);
 for (const apiFilePath of apiFilePaths) {
 	const realApiFilePath = resolve(apiFilePath);
 	try {
-		const apiModule = (await import(realApiFilePath)).default;
-		if (!apiModule) continue;
+		const apiModule = await import(realApiFilePath);
+		if (!apiModule.default) continue;
 		const matches = apiFilePath.match(matchApiFilePathRegex);
 		if (!matches) continue;
-		honoApp.on(matches[3]!, `/api${matches[1]!}`, apiModule);
+		const method = matches[3]!;
+		const urlPath = `/api${matches[1]!}`;
+		if (apiModule.zodOpenApiRouteConfig) {
+			if (!createZodOpenApiRoute) createZodOpenApiRoute = (await import('@hono/zod-openapi')).createRoute;
+			// @ts-expect-error
+			honoApp.openapi(createZodOpenApiRoute({ ...apiModule.zodOpenApiRouteConfig, path: urlPath.replace(/:(\w+?)(\/|$)/g, '{$1}$2'), method }), apiModule.default, apiModule.zodOpenApiRouteHook);
+		} else honoApp.on(method, urlPath, apiModule.default);
 		apisCount++;
 	} catch (error) {
 		// @ts-expect-error
