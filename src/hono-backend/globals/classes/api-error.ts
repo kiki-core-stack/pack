4be1f1aaa64e82d07032a84dc2ca7689d00d7bd7
kiki-kanri <a470666@gmail.com>
2024-10-11 16:30:48 +0800

    feat(hono-backend): add 200 and 500 status codes to `statusCodeToMessageMap`

--- src/hono-backend/globals/classes/api-error.ts
+++ src/hono-backend/globals/classes/api-error.ts
@@ -5,12 +5,14 @@ declare global {
 }
 
 export const statusCodeToMessageMap = Object.freeze<Dict<string>>({
+	200: '成功',
 	400: '資料格式錯誤或是非法操作！',
 	401: '尚未登入！',
 	403: '沒有權限！',
 	404: '找不到資料！',
 	409: '欄位值重複或是該資料使用中！',
-	413: '檔案大小超出限制！'
+	413: '檔案大小超出限制！',
+	500: '系統錯誤！'
 });
 
 class _ApiError<D extends object> extends Error {
