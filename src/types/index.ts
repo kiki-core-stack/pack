e50f00ca1830988e8144061129c78c67958fa094
kiki-kanri <a470666@gmail.com>
2024-10-12 21:58:06 +0800

    chore: remove all ajv dependencies, constants, and utils

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,10 +1,7 @@
-import type { JSONSchemaType } from 'ajv';
-
 export type {} from '@kikiutils/mongoose/types';
 export type {} from '@kikiutils/types';
 
 declare global {
-	type AjvValidatorJSONSchema<T, O extends Exclude<keyof T, 'id' | keyof TwoFactorAuthenticationCodesData> = never> = JSONSchemaType<OmitMongooseTimestampAndOtherFields<T, O | 'id' | keyof TwoFactorAuthenticationCodesData>>;
 	type TwoFactorAuthenticationMethod = 'emailOtp' | 'totp';
 	type TwoFactorAuthenticationStatus = Record<TwoFactorAuthenticationMethod, boolean>;
 }
