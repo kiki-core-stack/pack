96ee34c3832e8e1c6631589dfba6601c51f587c7
kiki-kanri <a470666@gmail.com>
2024-10-16 09:52:41 +0800

    refactor: update regex pattern for validating telegramSuperGroupId in zod validator

--- src/constants/zod/_index.ts
+++ src/constants/zod/_index.ts
@@ -5,4 +5,4 @@ export * from 'zod';
 
 export const objectId = () => _z.custom<string>((value) => Types.ObjectId.isValid(value), { message: 'Invalid ObjectId' });
 export const objectIdOrEmptyString = () => _z.custom<string>((value) => !value || Types.ObjectId.isValid(value), { message: 'Invalid ObjectId or empty string' });
-export const telegramSuperGroupId = () => _z.custom<string>((value) => /^-100[0-9]{10}$/.test(value), { message: 'Invalid Telegram Super Group Id' });
+export const telegramSuperGroupId = () => _z.custom<string>((value) => /^-100[\d]{10}$/.test(value), { message: 'Invalid Telegram Super Group Id' });
