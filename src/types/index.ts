e64957fbafe5eb2a4d0b1abba437e1d40bee282d
kiki-kanri <a470666@gmail.com>
2024-10-16 17:39:38 +0800

    feat: exclude `createdByAdmin` and `editedByAdmin` fields from ZodValidatorType keys

--- src/types/index.ts
+++ src/types/index.ts
@@ -6,8 +6,8 @@ export type {} from '@kikiutils/types';
 declare global {
 	type TwoFactorAuthenticationMethod = 'emailOtp' | 'totp';
 	type TwoFactorAuthenticationStatus = Record<TwoFactorAuthenticationMethod, boolean>;
-	type ZodValidatorType<Output = any, O extends Exclude<keyof Output, 'id' | keyof TwoFactorAuthenticationCodesData> = never, Def extends ZodTypeDef = ZodTypeDef> = ZodType<
-		OmitMongooseTimestampAndOtherFields<Output, O | 'id' | keyof TwoFactorAuthenticationCodesData>,
+	type ZodValidatorType<Output = any, O extends Exclude<keyof Output, 'createdByAdmin' | 'editedByAdmin' | 'id' | keyof TwoFactorAuthenticationCodesData> = never, Def extends ZodTypeDef = ZodTypeDef> = ZodType<
+		OmitMongooseTimestampAndOtherFields<Output, O | 'createdByAdmin' | 'editedByAdmin' | 'id' | keyof TwoFactorAuthenticationCodesData>,
 		Def
 	>;
 }
