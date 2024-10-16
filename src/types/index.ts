396a2f74c1fe4c7211deae246765f0ecfe023334
kiki-kanri <a470666@gmail.com>
2024-10-16 16:23:24 +0800

    feat: add global `ZodValidatorType` type

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,7 +1,14 @@
+import type { ZodType, ZodTypeDef } from 'zod';
+
 export type {} from '@kikiutils/mongoose/types';
 export type {} from '@kikiutils/types';
 
 declare global {
 	type TwoFactorAuthenticationMethod = 'emailOtp' | 'totp';
 	type TwoFactorAuthenticationStatus = Record<TwoFactorAuthenticationMethod, boolean>;
+	type ZodValidatorType<Output = any, O extends Exclude<keyof Output, 'id' | keyof TwoFactorAuthenticationCodesData> = never, Def extends ZodTypeDef = ZodTypeDef, Input = Output> = ZodType<
+		OmitMongooseTimestampAndOtherFields<Output, O | 'id' | keyof TwoFactorAuthenticationCodesData>,
+		Def,
+		Input
+	>;
 }
