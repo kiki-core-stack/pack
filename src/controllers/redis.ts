51f09dd7ec600d0cc8806b727a28b7fa0cfe312f
kiki-kanri <a470666@gmail.com>
2024-10-17 16:57:24 +0800

    refactor: modify the instantiation method of `redisController`

--- src/controllers/redis.ts
+++ src/controllers/redis.ts
@@ -1,21 +1,21 @@
 import { redisInstance } from '@/constants/redis';
 import type { AdminDocument } from '@/models';
 
-export const redisController = (() => ({
-	twoFactorAuthentication: (() => {
-		const createOperateFunctions = (getKeyFunction: (admin: AdminDocument) => string) => ({
-			del: async (admin: AdminDocument) => await redisInstance.del(getKeyFunction(admin)),
-			get: async (admin: AdminDocument) => await redisInstance.get(getKeyFunction(admin)),
-			async set(admin: AdminDocument, value: string, seconds?: number) {
-				if (seconds) await redisInstance.setex(getKeyFunction(admin), seconds, value);
-				else await redisInstance.set(getKeyFunction(admin), value);
-			},
-			ttl: async (admin: AdminDocument) => await redisInstance.ttl(getKeyFunction(admin))
-		});
+export const redisController = (() => {
+	const createOperateFunctions = <T>(getKeyFunction: (input: T) => string) => ({
+		del: async (input: T) => await redisInstance.del(getKeyFunction(input)),
+		get: async (input: T) => await redisInstance.get(getKeyFunction(input)),
+		async set(input: T, value: string, seconds?: number) {
+			if (seconds) await redisInstance.setex(getKeyFunction(input), seconds, value);
+			else await redisInstance.set(getKeyFunction(input), value);
+		},
+		ttl: async (input: T) => await redisInstance.ttl(getKeyFunction(input))
+	});
 
-		return {
+	return {
+		twoFactorAuthentication: {
 			emailOtpCode: createOperateFunctions((admin: AdminDocument) => `admin:emailOtpCode:${admin.id}:${admin.email}`),
 			tempTotpSecret: createOperateFunctions((admin: AdminDocument) => `admin:tempTotpSecret:${admin.id}`)
-		};
-	})()
-}))();
+		}
+	};
+})();
