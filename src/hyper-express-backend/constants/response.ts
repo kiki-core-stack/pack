024a4ed59910ec574a4f3916972bd94bbbb87da2
kiki-kanri <a470666@gmail.com>
2024-10-21 09:20:52 +0800

    feat: add 429 status to `statusCodeToResponseMessageMap`

--- src/hyper-express-backend/constants/response.ts
+++ src/hyper-express-backend/constants/response.ts
@@ -8,6 +8,7 @@ export const statusCodeToResponseMessageMap = Object.freeze<Record<number, strin
 	404: '找不到資料！',
 	409: '欄位值重複或是該資料使用中！',
 	413: '檔案大小超出限制！',
+	429: '請求過於頻繁！',
 	500: '系統錯誤！',
 	503: '系統維護中！',
 	504: '超時！'
