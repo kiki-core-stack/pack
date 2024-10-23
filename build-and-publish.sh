cdd5db402920240c217bfa1a52f479f63a6a1c51
kiki-kanri <a470666@gmail.com>
2024-10-23 11:39:07 +0800

    chore: modify build-and-publish.sh

--- build-and-publish.sh
+++ build-and-publish.sh
@@ -2,5 +2,4 @@
 
 cd "$(dirname "$(readlink -f "$0")")"
 . ./.env
-export NPM_CONFIG_REGISTRY
-pnpm run build && cp ./LICENSE ./package.json ./README.md ./dist && cd ./dist && npm publish
+pnpm run build && cp ./LICENSE ./package.json ./README.md ./dist && cd ./dist && npm publish --registry=$NPM_CONFIG_REGISTRY
