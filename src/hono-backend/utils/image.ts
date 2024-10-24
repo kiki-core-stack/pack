a413b661036e9b8b41867a9015bcb88706e21495
kiki-kanri <a470666@gmail.com>
2024-10-24 16:36:49 +0800

    refactor: rename `validateImageFile` to `validateImageFileMimeTypeAndSize` and enhance functionality

--- src/hono-backend/utils/image.ts
+++ src/hono-backend/utils/image.ts
@@ -1,14 +1,16 @@
 import type { PathLike } from '@kikiutils/classes/path';
 import type { Sharp, SharpOptions } from 'sharp';
 
-import { isAcceptedImageFile } from '@/utils/file';
+import { getAcceptedImageFileMimeType } from '@/utils/file';
 import { convertAndSaveImageFile } from '@/utils/image';
 
 export const convertAndSaveImageFileOrThrowError = async (file: Blob, savePath: PathLike, inputOptions?: SharpOptions, outputFormat: Parameters<Sharp['toFormat']>[0] = 'webp', outputOptions?: Parameters<Sharp['toFormat']>[1]) => {
 	if (!(await convertAndSaveImageFile(file, savePath, inputOptions, outputFormat, outputOptions))) throwAPIError(500);
 };
 
-export const validateImageFile = async (file: Blob, acceptGif?: boolean, ignoreFileSize?: boolean) => {
-	if (!(await isAcceptedImageFile(file, acceptGif))) throwAPIError(400);
+export const validateImageFileMimeTypeAndSize = async (file: Blob, acceptGif?: boolean, ignoreFileSize?: boolean) => {
+	const fileMimeType = await getAcceptedImageFileMimeType(file, acceptGif);
+	if (!fileMimeType) throwAPIError(400);
 	if (!ignoreFileSize && file.size > 1048576 * 16) throwAPIError(413); // Max 16MB
+	return fileMimeType;
 };
