35cdefbae2e29567eee1cdb847d7179148dfef2e
kiki-kanri <a470666@gmail.com>
2024-10-24 16:34:29 +0800

    refactor: rename `isAcceptedImageFile` to `getAcceptedImageFileMimeType` and enhance functionality

--- src/utils/file.ts
+++ src/utils/file.ts
@@ -2,10 +2,10 @@ import { WASMagic } from 'wasmagic';
 
 import { acceptedImageMimeTypes } from '@/constants/image';
 
-export const isAcceptedImageFile = async (file: Blob, acceptGif?: boolean) => {
+export const getAcceptedImageFileMimeType = async (file: Blob, acceptGif?: boolean) => {
 	const fileMimeType = await getFileMimeType(file);
-	if (acceptGif && fileMimeType === 'image/gif') return true;
-	return acceptedImageMimeTypes.includes(fileMimeType);
+	if ((fileMimeType === 'image/gif' && !acceptGif) || !acceptedImageMimeTypes.includes(fileMimeType)) return;
+	return fileMimeType;
 };
 
 export const getFileMimeType = async (file: Blob) => wasMagic.detect(new Uint8Array(await file.slice(0, 2048).arrayBuffer()));
