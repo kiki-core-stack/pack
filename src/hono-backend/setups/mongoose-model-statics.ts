1cfa4f5b3351cbee3026e2100fe88d7a82a47a5b
kiki-kanri <a470666@gmail.com>
2024-10-25 09:54:19 +0800

    refactor(hono-backend): update the method for configuring `customMongooseOptions`

--- src/hono-backend/setups/mongoose-model-statics.ts
+++ src/hono-backend/setups/mongoose-model-statics.ts
@@ -1,4 +1,4 @@
-import { customMongooseOptions } from '@kikiutils/mongoose/options';
+import { setCustomMongooseOptions } from '@kikiutils/mongoose/options';
 import type { Context } from 'hono';
 import { Types } from 'mongoose';
 import type { HydratedDocument, ProjectionType, QueryOptions, RootFilterQuery, Schema } from 'mongoose';
@@ -20,19 +20,20 @@ declare module '@kikiutils/mongoose/types' {
 	}
 }
 
-customMongooseOptions.beforeModelBuild = <DocType, Model extends BaseMongoosePaginateModel<DocType, InstanceMethodsAndOverrides, QueryHelpers>, InstanceMethodsAndOverrides = {}, QueryHelpers = {}>(
-	schema: Schema<DocType, Model, InstanceMethodsAndOverrides, QueryHelpers>
-) => {
-	schema.static('findByRouteId', function (ctx: Context, projection?: Nullable<ProjectionType<DocType>>, options?: Nullable<QueryOptions<DocType>>) {
-		return this.findById(ctx.req.param('id'), projection, options);
-	});
+setCustomMongooseOptions(
+	'beforeModelBuild',
+	<DocType, Model extends BaseMongoosePaginateModel<DocType, InstanceMethodsAndOverrides, QueryHelpers>, InstanceMethodsAndOverrides = {}, QueryHelpers = {}>(schema: Schema<DocType, Model, InstanceMethodsAndOverrides, QueryHelpers>) => {
+		schema.static('findByRouteId', function (ctx: Context, projection?: Nullable<ProjectionType<DocType>>, options?: Nullable<QueryOptions<DocType>>) {
+			return this.findById(ctx.req.param('id'), projection, options);
+		});
 
-	schema.static('findByRouteIdOrThrowNotFoundError', async function (ctx: Context, filterQuery?: RootFilterQuery<DocType>, projection?: Nullable<ProjectionType<DocType>>, options?: Nullable<QueryOptions<DocType>>) {
-		const id = ctx.req.param('id');
-		if (!id) throwAPIError(404);
-		if (!Types.ObjectId.isValid(id)) throwAPIError(400);
-		const document = await this.findOne({ ...filterQuery, _id: id }, projection, options);
-		if (!document) throwAPIError(404);
-		return document;
-	});
-};
+		schema.static('findByRouteIdOrThrowNotFoundError', async function (ctx: Context, filterQuery?: RootFilterQuery<DocType>, projection?: Nullable<ProjectionType<DocType>>, options?: Nullable<QueryOptions<DocType>>) {
+			const id = ctx.req.param('id');
+			if (!id) throwAPIError(404);
+			if (!Types.ObjectId.isValid(id)) throwAPIError(400);
+			const document = await this.findOne({ ...filterQuery, _id: id }, projection, options);
+			if (!document) throwAPIError(404);
+			return document;
+		});
+	}
+);
