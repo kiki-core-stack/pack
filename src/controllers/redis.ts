6f1b5dddb197c4f3ec06fd854f8d430e79182062
kiki-kanri <a470666@gmail.com>
2024-10-25 09:48:18 +0800

    refactor: move `createOperateFunctions` from `redisController` to outer scope

--- src/controllers/redis.ts
+++ src/controllers/redis.ts
@@ -1,21 +1,19 @@
 import { redisInstance } from '@/constants/redis';
 import type { AdminDocument } from '@/models/admin';
 
-export const redisController = (() => {
-	const createOperateFunctions = <T>(getKeyFunction: (input: T) => string) => ({
-		del: async (input: T) => await redisInstance.del(getKeyFunction(input)),
-		get: async (input: T) => await redisInstance.get(getKeyFunction(input)),
-		async set(input: T, value: string, seconds?: number) {
-			if (seconds) await redisInstance.setex(getKeyFunction(input), seconds, value);
-			else await redisInstance.set(getKeyFunction(input), value);
-		},
-		ttl: async (input: T) => await redisInstance.ttl(getKeyFunction(input))
-	});
+const createOperateFunctions = <T>(getKeyFunction: (input: T) => string) => ({
+	del: async (input: T) => await redisInstance.del(getKeyFunction(input)),
+	get: async (input: T) => await redisInstance.get(getKeyFunction(input)),
+	async set(input: T, value: string, seconds?: number) {
+		if (seconds) await redisInstance.setex(getKeyFunction(input), seconds, value);
+		else await redisInstance.set(getKeyFunction(input), value);
+	},
+	ttl: async (input: T) => await redisInstance.ttl(getKeyFunction(input))
+});
 
-	return {
-		twoFactorAuthentication: {
-			emailOTPCode: createOperateFunctions((admin: AdminDocument) => `admin:emailOTPCode:${admin.id}:${admin.email}`),
-			tempTOTPSecret: createOperateFunctions((admin: AdminDocument) => `admin:tempTOTPSecret:${admin.id}`)
-		}
-	};
-})();
+export const redisController = {
+	twoFactorAuthentication: {
+		emailOTPCode: createOperateFunctions((admin: AdminDocument) => `admin:emailOTPCode:${admin.id}:${admin.email}`),
+		tempTOTPSecret: createOperateFunctions((admin: AdminDocument) => `admin:tempTOTPSecret:${admin.id}`)
+	}
+};
