725046a2de1894a63b41077efd353082a6d4f296
kiki-kanri <a470666@gmail.com>
2024-10-28 16:13:05 +0800

    refactor: update custom Zod validation rule definition method

--- src/constants/zod/_index.ts
+++ src/constants/zod/_index.ts
@@ -3,6 +3,6 @@ import { z as _z } from 'zod';
 
 export * from 'zod';
 
-export const objectId = () => _z.custom<string>((value) => Types.ObjectId.isValid(value), { message: 'Invalid ObjectId' });
-export const objectIdOrEmptyString = () => _z.custom<string>((value) => !value || Types.ObjectId.isValid(value), { message: 'Invalid ObjectId or empty string' });
-export const telegramSuperGroupId = () => _z.custom<string>((value) => /^-100[\d]{10}$/.test(value), { message: 'Invalid Telegram Super Group Id' });
+export const objectId = () => _z.string().refine((value) => Types.ObjectId.isValid(value), { message: 'Invalid ObjectId' });
+export const objectIdOrEmptyString = () => _z.string().refine((value) => !value || Types.ObjectId.isValid(value), { message: 'Invalid ObjectId or empty string' });
+export const telegramSuperGroupId = () => _z.string().refine((value) => /^-100[\d]{10}$/.test(value), { message: 'Invalid Telegram Super Group Id' });
