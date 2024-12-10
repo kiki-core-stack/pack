ea0c5afe2b42887d583d379ab1505e0e83d7aef4
kiki-kanri <a470666@gmail.com>
2024-12-10 14:47:20 +0800

    refactor: remove && from scripts and add set -e at the top

--- build-and-publish.sh
+++ build-and-publish.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+set -e
 cd "$(dirname "$(readlink -f "$0")")"
 . ./.env
-pnpm run build && cp ./LICENSE ./package.json ./README.md ./dist && cd ./dist && npm publish --registry=$NPM_CONFIG_REGISTRY
+pnpm run build
+cp ./LICENSE ./package.json ./README.md ./dist
+cd ./dist
+npm publish --registry=$NPM_CONFIG_REGISTRY
--- fetch-and-merge.sh
+++ fetch-and-merge.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
+set -e
 git fetch https://github.com/kiki-core-stack/pack main
 git merge FETCH_HEAD
