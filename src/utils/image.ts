8c6b8ad4337fb5df6bede1c5a71653323f38835a
kiki-kanri <a470666@gmail.com>
2024-12-12 17:10:59 +0800

    chore: change permission value to `0o644` for `convertAndSaveImageFile` and add error log

--- src/utils/image.ts
+++ src/utils/image.ts
@@ -1,13 +1,17 @@
 import type { PathLike } from '@kikiutils/classes/path';
 import { chmod, remove } from '@kikiutils/fs-extra';
+import logger from '@kikiutils/node/consola';
 import sharp from 'sharp';
 import type { Sharp, SharpOptions } from 'sharp';
 
 export async function convertAndSaveImageFile(file: Blob, savePath: PathLike, inputOptions?: SharpOptions, outputFormat: Parameters<Sharp['toFormat']>[0] = 'webp', outputOptions?: Parameters<Sharp['toFormat']>[1]) {
     try {
         await sharp(await file.arrayBuffer(), inputOptions).toFormat(outputFormat, { quality: 75, ...outputOptions }).toFile(savePath.toString());
-        if (await chmod(savePath.toString(), 0o755)) return true;
-    } catch {}
+        if (await chmod(savePath.toString(), 0o644)) return true;
+    } catch (error) {
+        logger.error(error);
+    }
+
     remove(savePath.toString());
     return false;
 }
