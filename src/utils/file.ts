e4da948e156caef3499511d76681da0b21bfe516
kiki-kanri <a470666@gmail.com>
2024-12-27 11:21:09 +0800

    fix: fix the problem that `getFileMimeType` method return type is not `Promise<string>`

--- src/utils/file.ts
+++ src/utils/file.ts
@@ -2,8 +2,8 @@ import { WASMagic } from 'wasmagic';
 
 import { acceptedImageMimeTypes } from '@/constants/image';
 
-let wasMagic;
-export const getFileMimeType = async (file: Blob) => await (wasMagic ||= await WASMagic.create()).detect(new Uint8Array(await file.slice(0, 2048).arrayBuffer()));
+let wasMagic: undefined | WASMagic;
+export const getFileMimeType = async (file: Blob) => (wasMagic ||= await WASMagic.create()).detect(new Uint8Array(await file.slice(0, 2048).arrayBuffer()));
 
 export async function getAcceptedImageFileMimeType(file: Blob, acceptGif?: boolean) {
     const fileMimeType = await getFileMimeType(file);
