cd4ac64b4de136f6eb75d23f4d69c65ab0c3beb6
kiki-kanri <a470666@gmail.com>
2025-01-10 16:42:02 +0800

    refactor!: rename `src/utils/two-factor-authentication.ts` to `src/utils/otp.ts`

--- scripts/send-admin-email-otp-code.ts
+++ scripts/send-admin-email-otp-code.ts
@@ -1,9 +1,10 @@
-import '../src/hono-backend/globals/two-factor-authentication';
+import '../src/hono-backend/globals/otp';
 import { AdminModel } from '../src/models/admin';
 
 (async () => {
     const admin = await AdminModel.findByAccount('');
     if (!admin) throw new Error('找不到管理員！');
-    console.log(await sendEmailOTPCode(admin));
+    if (!admin.email) throw new Error('管理員沒有Email！');
+    console.log(await sendEmailOTPCode(admin.email, 'adminChangePassword', admin.id));
     process.exit();
 })();
