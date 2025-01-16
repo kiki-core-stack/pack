cf6ffb1b4b27c64c5695a1c3672f0049ef039f65
kiki-kanri <a470666@gmail.com>
2025-01-16 11:57:58 +0800

    refactor: modify definition and implementation of `SocketIoEvent` constant

--- src/constants/socket.io-event.ts
+++ src/constants/socket.io-event.ts
@@ -1,10 +1,28 @@
-// eslint-disable-next-line ts/no-namespace
-export namespace SocketIoEvent {
-    export enum ToAdminBackend {}
+type LowercaseUppercaseLetters<T extends string> = T extends `${infer First}${infer Rest}` ? First extends Uppercase<First> ? `${Lowercase<First>}${LowercaseUppercaseLetters<Rest>}` : LowercaseUppercaseLetters<Rest> : '';
 
-    export enum ToAdminFrontend {}
-
-    export enum ToServer {
-        Test = 's.t',
-    }
+function createEnumObject<N extends string, T extends string[]>(name: N, keys: T) {
+    const enumObject: Dict<string> = {};
+    new Set(keys).forEach((key) => enumObject[key] = `${name}.${key}`.replace(/[a-z]/g, '').toLowerCase());
+    return Object.freeze(enumObject as { [key in T[number]]: `${LowercaseUppercaseLetters<N>}.${LowercaseUppercaseLetters<key>}` });
 }
+
+export const ToAdminBackend = createEnumObject(
+    'ToAdminBackend',
+    [] as const,
+);
+
+export const ToAdminFrontend = createEnumObject(
+    'ToAdminFrontend',
+    [] as const,
+);
+
+export const ToServer = createEnumObject(
+    'ToServer',
+    ['Test'] as const,
+);
+
+export const SocketIoEvent = {
+    ToAdminBackend,
+    ToAdminFrontend,
+    ToServer,
+};
