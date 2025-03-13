ae95467bd41b6f4464708b9971a0ea1d81f607de
kiki-kanri <a470666@gmail.com>
2025-03-13 15:15:40 +0800

    refactor: remove `rollup-plugin-delete` and add custom `clean-empty-mjs` plugin

--- ts-project-builder.config.mjs
+++ ts-project-builder.config.mjs
@@ -1,18 +1,22 @@
-import del from 'rollup-plugin-delete';
+import {
+    readFile,
+    rm,
+} from 'node:fs/promises';
+import { join } from 'node:path';
 import { defineConfig } from 'ts-project-builder';
 
+function cleanEmptyMjsFiles() {
+    const processFile = async (filePath) => !(await readFile(filePath, 'utf-8')).trim() && rm(filePath, { force: true });
+    return {
+        name: 'clean-empty-mjs',
+        async writeBundle(_, bundle) {
+            const promises = Object.keys(bundle).map((fileName) => fileName.endsWith('.mjs') && processFile(join('./dist', fileName)).catch(() => {}));
+            await Promise.all(promises);
+        },
+    };
+}
+
 export default defineConfig({
-    additionalInputPlugins: {
-        afterBuiltIns: [
-            del({
-                hook: 'writeBundle',
-                targets: [
-                    './dist/libs/storage/types.mjs',
-                    './dist/models/**/types.mjs',
-                    './dist/types/**/*.mjs',
-                ],
-            }),
-        ],
-    },
+    additionalInputPlugins: { afterBuiltIns: [cleanEmptyMjsFiles()] },
     builtInInputPluginOptions: { nodeExternal: { devDeps: true } },
 });
