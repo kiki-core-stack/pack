ae52aed49514498b5ef525ef9bbf87e2c9b87c8e
kiki-kanri <a470666@gmail.com>
2025-03-25 15:14:07 +0800

    feat: add `assertUpdateOneByRouteId` to hono backend mongoose model statics

--- src/hono-backend/setups/mongoose-model-statics.ts
+++ src/hono-backend/setups/mongoose-model-statics.ts
@@ -3,14 +3,25 @@ import type { Context } from 'hono';
 import { Types } from 'mongoose';
 import type {
     HydratedDocument,
+    mongo,
+    MongooseUpdateQueryOptions,
     ProjectionType,
     QueryOptions,
     RootFilterQuery,
     Schema,
+    UpdateQuery,
 } from 'mongoose';
 
 declare module '@kikiutils/mongoose/types' {
     interface BaseModelStatics<RawDocType, InstanceMethodsAndOverrides = object, QueryHelpers = object> {
+        assertUpdateOneByRouteId: (
+            ctx: Context,
+            updateQuery: UpdateQuery<RawDocType>,
+            options?: Nullable<(mongo.UpdateOptions & MongooseUpdateQueryOptions<RawDocType>)>,
+            filterQuery?: RootFilterQuery<RawDocType>,
+            expectedModifiedCount?: number
+        ) => Promise<void>;
+
         findByRouteId: (
             ctx: Context,
             projection?: Nullable<ProjectionType<RawDocType>>,
@@ -39,6 +50,35 @@ setCustomMongooseOptions(
         InstanceMethodsAndOverrides = object,
         QueryHelpers = object,
     >(schema: Schema<DocType, Model, InstanceMethodsAndOverrides, QueryHelpers>) => {
+        schema.static(
+            'assertUpdateOneByRouteId',
+            async function (
+                ctx: Context,
+                updateQuery: UpdateQuery<DocType>,
+                options?: Nullable<(mongo.UpdateOptions & MongooseUpdateQueryOptions<DocType>)>,
+                filterQuery?: RootFilterQuery<DocType>,
+                expectedModifiedCount: number = 1,
+            ) {
+                const id = ctx.req.param('id');
+                if (!id) throwApiError(404);
+                const updateResult = await this.updateOne(
+                    {
+                        ...filterQuery,
+                        _id: id,
+                    },
+                    updateQuery,
+                    options,
+                );
+
+                if (!updateResult.acknowledged) throw new Error('Update was not acknowledged.');
+                if (updateResult.matchedCount !== 1) throwApiError(404);
+                if (updateResult.modifiedCount < expectedModifiedCount) {
+                    // eslint-disable-next-line style/max-len
+                    throw new Error(`Expected to modify at least ${expectedModifiedCount} document(s), but modified ${updateResult.modifiedCount}.`);
+                }
+            },
+        );
+
         schema.static(
             'findByRouteId',
             function (
