26d74b188cf916b2b9735615c2e4905c7d9de3ae
kiki-kanri <a470666@gmail.com>
2025-04-01 11:16:51 +0800

    feat: update `ApiResponseData` type and modify `api-requests` functions to support `errorCode` field type configuration

--- src/libs/api-requests.ts
+++ src/libs/api-requests.ts
@@ -13,96 +13,97 @@ export type ApiRequestParams = Dict<any>;
 
 export const apiInstance: AxiosInstance = axios.create();
 
-export function deleteApi<T extends object = object>(
+export function deleteApi<T extends object | undefined = undefined, E extends string | undefined = undefined>(
     url: string,
     params?: ApiRequestParams,
     config?: AxiosRequestConfig,
 ) {
-    return requestApi<T>(url, 'delete', params, {}, config);
+    return requestApi<T, E>(url, 'delete', params, {}, config);
 }
 
-export async function $deleteApi<T extends object = object>(
+export async function $deleteApi<T extends object | undefined = undefined, E extends string | undefined = undefined>(
     url: string,
     params?: ApiRequestParams,
     config?: AxiosRequestConfig,
 ) {
-    return (await deleteApi<T>(url, params, config))?.data;
+    return (await deleteApi<T, E>(url, params, config))?.data;
 }
 
-export function getApi<T extends object = object>(
+export function getApi<T extends object | undefined = undefined, E extends string | undefined = undefined>(
     url: string,
     params?: ApiRequestParams,
     config?: AxiosRequestConfig,
 ) {
-    return requestApi<T>(url, 'get', params, {}, config);
+    return requestApi<T, E>(url, 'get', params, {}, config);
 }
 
-export async function $getApi<T extends object = object>(
+export async function $getApi<T extends object | undefined = undefined, E extends string | undefined = undefined>(
     url: string,
     params?: ApiRequestParams,
     config?: AxiosRequestConfig,
 ) {
-    return (await getApi<T>(url, params, config))?.data;
+    return (await getApi<T, E>(url, params, config))?.data;
 }
 
-export function patchApi<T extends object = object>(
+export function patchApi<T extends object | undefined = undefined, E extends string | undefined = undefined>(
     url: string,
     data?: ApiRequestData,
     config?: AxiosRequestConfig<ApiRequestData>,
 ) {
-    return requestApi<T>(url, 'patch', {}, data, config);
+    return requestApi<T, E>(url, 'patch', {}, data, config);
 }
 
-export async function $patchApi<T extends object = object>(
+export async function $patchApi<T extends object | undefined = undefined, E extends string | undefined = undefined>(
     url: string,
     data?: ApiRequestData,
     config?: AxiosRequestConfig<ApiRequestData>,
 ) {
-    return (await patchApi<T>(url, data, config))?.data;
+    return (await patchApi<T, E>(url, data, config))?.data;
 }
 
-export function postApi<T extends object = object>(
+export function postApi<T extends object | undefined = undefined, E extends string | undefined = undefined>(
     url: string,
     data?: ApiRequestData,
     config?: AxiosRequestConfig<ApiRequestData>,
 ) {
-    return requestApi<T>(url, 'post', {}, data, config);
+    return requestApi<T, E>(url, 'post', {}, data, config);
 }
 
-export async function $postApi<T extends object = object>(
+export async function $postApi<T extends object | undefined = undefined, E extends string | undefined = undefined>(
     url: string,
     data?: ApiRequestData,
     config?: AxiosRequestConfig<ApiRequestData>,
 ) {
-    return (await postApi<T>(url, data, config))?.data;
+    return (await postApi<T, E>(url, data, config))?.data;
 }
 
-export function putApi<T extends object = object>(
+export function putApi<T extends object | undefined = undefined, E extends string | undefined = undefined>(
     url: string,
     data?: ApiRequestData,
     config?: AxiosRequestConfig<ApiRequestData>,
 ) {
-    return requestApi<T>(url, 'put', {}, data, config);
+    return requestApi<T, E>(url, 'put', {}, data, config);
 }
 
-export async function $putApi<T extends object = object>(
+export async function $putApi<T extends object | undefined = undefined, E extends string | undefined = undefined>(
     url: string,
     data?: ApiRequestData,
     config?: AxiosRequestConfig<ApiRequestData>,
 ) {
-    return (await putApi<T>(url, data, config))?.data;
+    return (await putApi<T, E>(url, data, config))?.data;
 }
 
 export function requestApi<
-    T extends object = object,
-    R extends AxiosResponse<ApiResponseData<T>> | undefined = AxiosResponse<ApiResponseData<T>> | undefined,
+    T extends object | undefined = undefined,
+    E extends string | undefined = undefined,
+    R extends AxiosResponse<ApiResponseData<T, E>> | undefined = AxiosResponse<ApiResponseData<T, E>> | undefined,
     D extends ApiRequestData = any,
 >(
     url: string,
     method: Method,
     params?: ApiRequestData,
     data?: D,
-    config?: AxiosRequestConfig,
+    config?: AxiosRequestConfig<D>,
 ) {
     return apiInstance.request<T, R, D>({
         ...config,
--- src/types/data/index.ts
+++ src/types/data/index.ts
@@ -1,13 +1,12 @@
 export type {} from '@kikiutils/mongoose/types/data';
 
 declare global {
-    type ApiResponseData<D extends object | undefined = undefined, E extends string | undefined = undefined> =
-        & (D extends object ? { data: D } : { data?: undefined })
-        & (E extends string ? { errorCode: E } : { errorCode?: undefined })
-        & {
-            message?: string;
-            success: boolean;
-        };
+    interface ApiResponseData<D extends object | undefined = undefined, E extends string | undefined = undefined> {
+        data?: D;
+        errorCode?: E;
+        message?: string;
+        success: boolean;
+    }
 
     interface TableRowData {
         id: string;
