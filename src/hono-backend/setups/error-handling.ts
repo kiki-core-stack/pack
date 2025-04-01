36ecb4de9882f6a060e770350f01c5d6efdfc33b
kiki-kanri <a470666@gmail.com>
2025-04-01 11:37:55 +0800

    feat: add `errorCode` field to response message when handling `ApiError` in Hono app onError

--- src/hono-backend/setups/error-handling.ts
+++ src/hono-backend/setups/error-handling.ts
@@ -45,6 +45,7 @@ export function setupHonoAppErrorHandling(honoApp: Hono) {
             return ctx.body(
                 JSON.stringify({
                     data: error.data,
+                    errorCode: error.errorCode,
                     message: error.message,
                     success: false,
                 }),
