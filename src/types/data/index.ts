61932c09e5ce401182f5f7fb5bfddf3fea085105
kiki-kanri <a470666@gmail.com>
2025-04-01 10:39:05 +0800

    feat: optimize `ApiResponseData` type for correct field presence and add `errorCode` field

--- src/types/data/index.ts
+++ src/types/data/index.ts
@@ -1,11 +1,13 @@
 export type {} from '@kikiutils/mongoose/types/data';
 
 declare global {
-    interface ApiResponseData<D extends object = object> {
-        data?: D;
-        message?: string;
-        success: boolean;
-    }
+    type ApiResponseData<D extends object | undefined = undefined, E extends string | undefined = undefined> =
+        & (D extends object ? { data: D } : { data?: undefined })
+        & (E extends string ? { errorCode: E } : { errorCode?: undefined })
+        & {
+            message?: string;
+            success: boolean;
+        };
 
     interface TableRowData {
         id: string;
