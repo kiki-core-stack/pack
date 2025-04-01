964f8822ec75e24fa1af5243213814d323d5bf4b
kiki-kanri <a470666@gmail.com>
2025-04-01 12:00:54 +0800

    refactor: simplify `createApiSuccessResponseData`
    - fix parameter order
    - set correct return type

--- src/hono-backend/libs/api/index.ts
+++ src/hono-backend/libs/api/index.ts
@@ -1,22 +1,17 @@
 import type { ContentfulStatusCode } from 'hono/utils/http-status';
+import type {
+    SetFieldType,
+    SetRequired,
+} from 'type-fest';
 
 import { ApiError } from './error';
 
-export function createApiSuccessResponseData(): ApiResponseData;
-export function createApiSuccessResponseData<D extends object>(data: D, message?: string): ApiResponseData<D>;
-export function createApiSuccessResponseData<D extends object>(message: string, data?: D): ApiResponseData<D>;
-export function createApiSuccessResponseData(arg1?: any, arg2?: any) {
-    if (typeof arg1 === 'string') {
-        const message = arg1;
-        arg1 = arg2;
-        arg2 = message;
-    }
-
+export function createApiSuccessResponseData<D extends object | undefined = undefined>(data?: D, message?: string) {
     return {
-        data: arg1,
-        message: arg2 ?? '成功',
+        data,
+        message: message ?? '成功',
         success: true,
-    };
+    } as SetRequired<SetFieldType<ApiResponseData<D>, 'success', true>, 'data' | 'message'>;
 }
 
 export function throwApiError<D extends object | undefined = undefined, E extends string | undefined = undefined>(
