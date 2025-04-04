1b4b3402f1f93974715f9ad0fef3036800fdcb5b
kiki-kanri <a470666@gmail.com>
2025-04-04 14:24:54 +0800

    refactor: move files from `src/models/admin/index` to its parent directory

--- src/models/admin/index.ts
+++ src/models/admin/index.ts
@@ -1,1 +1,13 @@
-export * from './index/';
+import { buildMongooseModel } from '@kikiutils/mongoose/utils';
+
+import { adminSchema } from './schema';
+import type {
+    Admin,
+    AdminMethodsAndOverrides,
+    AdminModel,
+} from './types';
+
+export type * from './types';
+
+const model = buildMongooseModel<Admin, AdminModel, AdminMethodsAndOverrides>('admin.admins', 'Admin', adminSchema);
+export { model as AdminModel };
