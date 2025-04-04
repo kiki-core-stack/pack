f2b6decaed4d674b84e3a06f7247aa78f910597a
kiki-kanri <a470666@gmail.com>
2025-04-04 14:45:00 +0800

    refactor: omit `message` when creating `ApiError` in `defaultApiErrors` to use internal default

--- src/hono-backend/constants/api.ts
+++ src/hono-backend/constants/api.ts
@@ -2,6 +2,6 @@ import { ApiError } from '../libs/api/error';
 import { defineApiErrorMapByErrorCode } from '../libs/api/index';
 
 export const defaultApiErrors = defineApiErrorMapByErrorCode([
-    new ApiError(400, '資料格式錯誤或非法操作！', 'badRequest'),
-    new ApiError(500, '系統錯誤！', 'internalServerError'),
+    new ApiError(400, undefined, 'badRequest'),
+    new ApiError(500, undefined, 'internalServerError'),
 ]);
