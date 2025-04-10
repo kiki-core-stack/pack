aab8de58ea4076efda0b7c5ffbe594e176908fcf
kiki-kanri <a470666@gmail.com>
2025-04-10 12:19:35 +0800

    refactor: remove alias used in zod import

--- src/constants/zod/_index.ts
+++ src/constants/zod/_index.ts
@@ -1,14 +1,14 @@
 import { Types } from 'mongoose';
-import { z as _z } from 'zod';
+import { z } from 'zod';
 
 export * from 'zod';
 
 export function objectId() {
-    return _z.string().refine((value) => Types.ObjectId.isValid(value), { message: 'Invalid ObjectId' });
+    return z.string().refine((value) => Types.ObjectId.isValid(value), { message: 'Invalid ObjectId' });
 }
 
 export function objectIdOrEmptyString() {
-    return _z
+    return z
         .string()
         .refine(
             (value) => !value || Types.ObjectId.isValid(value),
@@ -17,5 +17,5 @@ export function objectIdOrEmptyString() {
 }
 
 export function telegramSuperGroupId() {
-    return _z.string().refine((value) => /^-100\d{10}$/.test(value), { message: 'Invalid Telegram Super Group Id' });
+    return z.string().refine((value) => /^-100\d{10}$/.test(value), { message: 'Invalid Telegram Super Group Id' });
 }
