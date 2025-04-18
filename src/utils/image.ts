159b2f69996a29f2fdbda3ba1174c95aff776a6f
kiki-kanri <a470666@gmail.com>
2025-04-18 09:45:05 +0800

    refactor!: remove `saveConvertedImage` utils

--- src/utils/image.ts
+++ src/utils/image.ts
@@ -1,8 +1,6 @@
 import { Blob } from 'node:buffer';
 import type { Buffer } from 'node:buffer';
 
-import { Path } from '@kikiutils/classes/path';
-import type { PathLike } from '@kikiutils/classes/path';
 // @ts-expect-error Ignore this error.
 import sharp from 'sharp';
 import type {
@@ -31,20 +29,4 @@ export async function convertImage(
             )
             .toBuffer();
     } catch {}
-}
-
-export async function saveConvertedImage(
-    input: Blob | Buffer,
-    savePath: PathLike,
-    inputOptions?: SharpOptions,
-    outputFormat: Parameters<Sharp['toFormat']>[0] = 'webp',
-    outputOptions?: Parameters<Sharp['toFormat']>[1],
-) {
-    try {
-        savePath = Path.resolve(savePath);
-        const buffer = await convertImage(input, inputOptions, outputFormat, outputOptions);
-        if (buffer && await savePath.writeFile(buffer) && await savePath.chmod(0o644)) return true;
-        savePath.remove();
-    } catch {}
-    return false;
 }
