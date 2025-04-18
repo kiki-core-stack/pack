f446d1e291e3129c2bb18fb0f775ba6eaa860b43
kiki-kanri <a470666@gmail.com>
2025-04-18 14:19:14 +0800

    feat: add `BaseFileData` type

--- src/types/data/file.ts
+++ src/types/data/file.ts
@@ -1,5 +1,7 @@
 import type { FileStorageProvider } from '@/constants/file';
 
+export type BaseFileData = Pick<FileData, 'path' | 'provider'>;
+
 export interface FileData extends BaseMongooseModelData<true, false> {
     hash: string;
     path: string;
