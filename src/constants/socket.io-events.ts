7a8196c30113b431a7e357f8b6ee4569f25580b6
kiki-kanri <a470666@gmail.com>
2025-05-12 14:39:33 +0800

    chore: add `/* @__PURE__ */` annotation to each export in `socket.io-events` constants file

--- src/constants/socket.io-events.ts
+++ src/constants/socket.io-events.ts
@@ -7,17 +7,17 @@ function createEnumObject<N extends string, T extends string[]>(name: N, keys: T
     return enumObject as { [key in T[number]]: `${LowercaseUppercaseLetters<N>}.${LowercaseUppercaseLetters<key>}` };
 }
 
-export const ToAdminBackend = createEnumObject(
+export const ToAdminBackend = /* @__PURE__ */ createEnumObject(
     'ToAdminBackend',
     [] as const,
 );
 
-export const ToAdminFrontend = createEnumObject(
+export const ToAdminFrontend = /* @__PURE__ */ createEnumObject(
     'ToAdminFrontend',
     [] as const,
 );
 
-export const ToServer = createEnumObject(
+export const ToServer = /* @__PURE__ */ createEnumObject(
     'ToServer',
     ['Test'] as const,
 );
