794ed3792375281edafba0b6633560e4b6c76ce0
kiki-kanri <a470666@gmail.com>
2025-05-12 14:33:04 +0800

    feat: add `transform` to `objectId` and `objectIdOrEmptyString` Zod schema builder to convert to `ObjectId`

--- src/libs/zod/object-id.ts
+++ src/libs/zod/object-id.ts
@@ -2,14 +2,15 @@ import { Types } from 'mongoose';
 import * as z from 'zod';
 
 export function objectId() {
-    return z.string().refine((value) => Types.ObjectId.isValid(value), { message: 'Invalid ObjectId' });
+    return z
+        .string()
+        .refine((value) => Types.ObjectId.isValid(value), 'Invalid ObjectId')
+        .transform((value) => new Types.ObjectId(value));
 }
 
 export function objectIdOrEmptyString() {
     return z
         .string()
-        .refine(
-            (value) => !value || Types.ObjectId.isValid(value),
-            { message: 'Invalid ObjectId or empty string' },
-        );
+        .refine((value) => !value || Types.ObjectId.isValid(value), 'Invalid ObjectId or empty string')
+        .transform((value) => value ? new Types.ObjectId(value) : '');
 }
