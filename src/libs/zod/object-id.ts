31d0af8da62175ed0af9cf9e2e16740b4b0790c5
kiki-kanri <a470666@gmail.com>
2025-05-14 10:54:40 +0800

    chore: update `objectId` and `objectIdOrEmptyString` Zod schema refine messages to include periods

--- src/libs/zod/object-id.ts
+++ src/libs/zod/object-id.ts
@@ -4,13 +4,13 @@ import * as z from 'zod';
 export function objectId() {
     return z
         .string()
-        .refine((value) => Types.ObjectId.isValid(value), 'Invalid ObjectId')
+        .refine((value) => Types.ObjectId.isValid(value), 'Invalid ObjectId.')
         .transform((value) => new Types.ObjectId(value));
 }
 
 export function objectIdOrEmptyString() {
     return z
         .string()
-        .refine((value) => !value || Types.ObjectId.isValid(value), 'Invalid ObjectId or empty string')
+        .refine((value) => !value || Types.ObjectId.isValid(value), 'Invalid ObjectId or empty string.')
         .transform((value) => value ? new Types.ObjectId(value) : '');
 }
