24c5d5041521e1e6228ea444e07ade8669130add
kiki-kanri <a470666@gmail.com>
2025-05-21 13:48:17 +0800

    chore: modify `build-and-publish.sh` script

--- build-and-publish.sh
+++ build-and-publish.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 
 set -e
-cd "$(dirname "$(readlink -f "$0")")"
+
+SCRIPT_DIR="$(dirname "$(realpath "${BASH_SOURCE[0]}")")"
+cd "${SCRIPT_DIR}"
+
 . ./.env
-[ -z "$NPM_CONFIG_REGISTRY" ] && echo "Error: The environment variable 'NPM_CONFIG_REGISTRY' is not set!" && exit 1
+
+if [ -z "${NPM_CONFIG_REGISTRY}" ]; then
+    echo "Error: The environment variable 'NPM_CONFIG_REGISTRY' is not set!"
+    exit 1
+fi
+
 pnpm run build
-npm publish --registry=$NPM_CONFIG_REGISTRY
+npm publish --registry=${NPM_CONFIG_REGISTRY}
