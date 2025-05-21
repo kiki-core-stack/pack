7e3a9421dc8b5edbbac42e9142eb35a239048047
kiki-kanri <a470666@gmail.com>
2025-05-21 13:39:46 +0800

    refactor: remove unnecessary exports

--- src/constants/mongoose.ts
+++ src/constants/mongoose.ts
@@ -1,7 +1,5 @@
 import { ref } from '@kikiutils/mongoose/schema-builders';
 
-export { mongooseConnections } from '@kikiutils/mongoose/constants';
-
 export const mongooseRefSchemas = {
     admin: {
         nonRequired: ref('Admin').nonRequired,
