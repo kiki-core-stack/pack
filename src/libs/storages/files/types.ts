64b7cdab49b8155227ddda731a5f1c79e525443e
kiki-kanri <a470666@gmail.com>
2025-05-22 13:50:03 +0800

    chore: lint code

--- src/libs/storages/files/types.ts
+++ src/libs/storages/files/types.ts
@@ -1,11 +1,11 @@
 import type { FileStorageProvider } from '../../../constants/file';
 
-export interface LocalFileStorageConfig {
-    basePath: string;
-}
-
 export interface FileStorageUploadResult {
     hash: string;
     path: string;
     provider: FileStorageProvider;
+}
+
+export interface LocalFileStorageConfig {
+    basePath: string;
 }
