c2ddd4eb4ea689f0ba1b7a5c481e449c6457b03f
kiki-kanri <a470666@gmail.com>
2025-05-23 11:27:34 +0800

    feat: add caching for `FileModel.find` and extract shared logic from `find` and `findOne` middleware into a reusable function

--- src/models/file.ts
+++ src/models/file.ts
@@ -1,7 +1,11 @@
 import * as s from '@kikiutils/mongoose/schema-builders';
 import { buildMongooseModel } from '@kikiutils/mongoose/utils';
 import { getEnumNumberValues } from '@kikiutils/shared/enum';
-import { Schema } from 'mongoose';
+import {
+    Schema,
+    Types,
+} from 'mongoose';
+import type { Query } from 'mongoose';
 
 import { FileStorageProvider } from '../constants/file';
 import { schemaTimestampsConfigOnlyCreatedAt } from '../constants/mongoose';
@@ -22,19 +26,74 @@ const schema = new Schema<File, FileModel>(
     { timestamps: schemaTimestampsConfigOnlyCreatedAt },
 );
 
+function isEligibleIdQueryWithoutProjection(query: Query<any, any>, mode: 'in' | 'single') {
+    const filter = query.getFilter();
+    if (Object.keys(filter).length !== 1 || !filter._id) return false;
+    const projection = query.projection();
+    if (projection && Object.keys(projection).length > 0) return false;
+
+    if (mode === 'single') return typeof filter._id === 'string' || filter._id instanceof Types.ObjectId;
+    return (
+        typeof filter._id === 'object'
+        && Object.keys(filter._id).length === 1
+        && Array.isArray(filter._id.$in)
+    );
+}
+
+// find
+schema.post(
+    'find',
+    async function (result) {
+        if (this._mongooseOptions.isFromCache) return;
+        if (!isEligibleIdQueryWithoutProjection(this, 'in')) return;
+
+        await Promise.all(
+            result.map(async (document: any) => {
+                const fileDocumentData = document.toObject();
+                const id = document._id.toHexString();
+                lruStore.fileDocumentData.setItem(fileDocumentData, id);
+                await enhancedRedisStore.fileDocumentData.setItemWithTtl(3600, fileDocumentData, id);
+            }),
+        );
+    },
+);
+
+schema.pre(
+    'find',
+    async function () {
+        const filter = this.getFilter();
+        if (!isEligibleIdQueryWithoutProjection(this, 'in')) return;
+
+        const ids = (filter._id.$in as (string | Types.ObjectId)[]).map((id) => {
+            if (id instanceof Types.ObjectId) return id.toHexString();
+            return id;
+        });
+
+        const allFileDocumentData = await Promise.all(
+            ids.map((id) => lruStore.fileDocumentData.getItem(id) || enhancedRedisStore.fileDocumentData.getItem(id)),
+        );
+
+        if (!allFileDocumentData.includes(null)) {
+            this._mongooseOptions.isFromCache = true;
+            const model = this.model;
+            (this as any)._find = function () {
+                // eslint-disable-next-line new-cap
+                return allFileDocumentData.map((fileDocumentData) => new model(fileDocumentData));
+            };
+        }
+    },
+);
+
+// fineOne
 schema.post(
     'findOne',
     async function (result) {
-        if (this._mongooseOptions.isFromCache) return;
+        if (!isEligibleIdQueryWithoutProjection(this, 'single')) return;
 
-        const filter = this.getFilter();
-        if (Object.keys(filter).length !== 1 || !filter._id) return;
-        const projection = this.projection();
-        if (projection && Object.keys(projection).length) return;
-
-        const id = filter._id;
-        lruStore.fileDocumentData.setItem(result.toObject(), id);
-        await enhancedRedisStore.fileDocumentData.setItemWithTtl(3600, result.toObject(), id);
+        const fileDocumentData = result.toObject();
+        const id = result._id.toHexString();
+        lruStore.fileDocumentData.setItem(fileDocumentData, id);
+        await enhancedRedisStore.fileDocumentData.setItemWithTtl(3600, fileDocumentData, id);
     },
 );
 
@@ -42,18 +101,17 @@ schema.pre(
     'findOne',
     async function () {
         const filter = this.getFilter();
-        if (Object.keys(filter).length !== 1 || !filter._id) return;
-        const projection = this.projection();
-        if (projection && Object.keys(projection).length) return;
+        if (!isEligibleIdQueryWithoutProjection(this, 'single')) return;
 
         const id = filter._id;
         let fileDocumentData = lruStore.fileDocumentData.getItem(id);
         if (!fileDocumentData) fileDocumentData = await enhancedRedisStore.fileDocumentData.getItem(id);
         if (fileDocumentData) {
             this._mongooseOptions.isFromCache = true;
+            const model = this.model;
             (this as any)._findOne = function () {
                 // eslint-disable-next-line new-cap
-                return new this.model(fileDocumentData);
+                return new model(fileDocumentData);
             };
         }
     },
