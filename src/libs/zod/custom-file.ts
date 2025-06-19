b65e65da9f5207031888d75c15adb24372d810a3
kiki-kanri <a470666@gmail.com>
2025-06-19 17:12:29 +0800

    refactor: rewrite custom `ZodCustomFile` schema using Zod class `extend` pattern

--- src/libs/zod/custom-file.ts
+++ src/libs/zod/custom-file.ts
@@ -1,26 +1,38 @@
-import { Blob } from 'node:buffer';
+import { Blob as BufferBlob } from 'node:buffer';
 
-import * as z from 'zod/v4';
 import { ZodType } from 'zod/v4';
+import type { _ZodType } from 'zod/v4';
+import {
+    $constructor,
+    $ZodCustom,
+    _maxSize,
+    _minSize,
+    util,
+} from 'zod/v4/core';
 import type {
-    ZodCustom,
-    ZodPipe,
-    ZodTransform,
-} from 'zod/v4';
+    $ZodCheckMaxSizeParams,
+    $ZodCheckMinSizeParams,
+    $ZodCustomInternals,
+    $ZodCustomParams,
+} from 'zod/v4/core';
 
 import { getFileMimeType } from '../../utils/file';
 
-interface ZodCustomFile extends ZodPipe<ZodCustom<Blob, Blob>, ZodTransform<Blob, Blob>> {
-    commonImages: () => ZodCustomFile;
-    gif: () => ZodCustomFile;
-    jpeg: () => ZodCustomFile;
-    maxSize: (bytes: number) => ZodCustomFile;
-    maxSizeKb: (kb: number) => ZodCustomFile;
-    maxSizeMb: (mb: number) => ZodCustomFile;
-    mimeType: (mimeType: string) => ZodCustomFile;
-    mimeTypes: (mimeTypes: string[]) => ZodCustomFile;
-    png: () => ZodCustomFile;
-    webp: () => ZodCustomFile;
+interface ZodCustomFile extends
+    $ZodCustom<BufferBlob, BufferBlob>,
+    _ZodType<$ZodCustomInternals<BufferBlob, BufferBlob>> {
+    commonImages: () => this;
+    gif: () => this;
+    jpeg: () => this;
+    maxSize: (bytes: number, params?: $ZodCheckMaxSizeParams | string) => this;
+    maxSizeKb: (kb: number, params?: $ZodCheckMaxSizeParams | string) => this;
+    maxSizeMb: (mb: number, params?: $ZodCheckMaxSizeParams | string) => this;
+    mimeType: (...mimeTypes: Readonlyable<Arrayable<string>>[]) => this;
+    minSize: (bytes: number, params?: $ZodCheckMinSizeParams | string) => this;
+    minSizeKb: (kb: number, params?: $ZodCheckMinSizeParams | string) => this;
+    minSizeMb: (mb: number, params?: $ZodCheckMinSizeParams | string) => this;
+    png: () => this;
+    webp: () => this;
 }
 
 const commonImageMimeTypes: readonly string[] = [
@@ -33,115 +45,65 @@ const commonImageMimeTypes: readonly string[] = [
     'image/webp',
 ];
 
-export function customFile() {
-    const allowedMimeTypes = new Set<string>();
-    let detectedMimeType: string | undefined;
-    let maxBytes: number | undefined;
-    const zodCustomFile = z
-        .instanceof(Blob)
-        .check(async (ctx) => {
-            detectedMimeType = await getFileMimeType(ctx.value);
-            if (!detectedMimeType || !allowedMimeTypes.has(detectedMimeType)) {
-                ctx.issues.push({
-                    code: 'custom',
-                    input: ctx.value,
-                    message: `Invalid MIME type: ${detectedMimeType}.`,
-                });
-            }
+// eslint-disable-next-line style/max-len
+// TODO: Dealing with the problem of multiple MIME acquisitions with a single parse using the MIME type check multiple times
+export const ZodCustomFile: $constructor<ZodCustomFile> = /* @__PURE__ */ $constructor(
+    'ZodCustomFile',
+    (inst, def) => {
+        $ZodCustom.init(inst, def);
+        ZodType.init(inst, def);
 
-            if (maxBytes !== undefined && ctx.value.size > maxBytes) {
-                ctx.issues.push({
-                    code: 'custom',
-                    input: ctx.value,
-                    message: `File size must be less than ${maxBytes} bytes.`,
-                    params: { reason: 'fileTooLarge' },
-                });
-            }
-        })
-        .transform((file) => {
-            if (file.type === detectedMimeType) return file;
-            return new Blob([file], { type: detectedMimeType });
-        });
+        const params = def.params as { _allowedMimeTypes: Set<string> };
 
-    function decorate(schema: any): ZodCustomFile {
-        const clonedSchema = Object.create(Object.getPrototypeOf(schema));
-        const descriptors = Object.getOwnPropertyDescriptors(schema);
-        for (const [key, descriptor] of Object.entries(descriptors)) {
-            Object.defineProperty(
-                clonedSchema,
-                key,
-                {
-                    ...descriptor,
-                    configurable: true,
-                },
-            );
+        function createCheckMimeTypesFunction(...mimeTypes: Readonlyable<Arrayable<string>>[]) {
+            return () => {
+                [mimeTypes].flat(2).forEach((mimeType) => params._allowedMimeTypes.add(mimeType));
+                return inst.check(async (ctx) => {
+                    const detectedMimeType = await getFileMimeType(ctx.value);
+                    if (!detectedMimeType || !params._allowedMimeTypes.has(detectedMimeType)) {
+                        ctx.issues.push({
+                            code: 'custom',
+                            input: ctx.value,
+                            // eslint-disable-next-line style/max-len
+                            message: `Invalid MIME type: ${detectedMimeType}. Allowed types: ${[...params._allowedMimeTypes].join(', ')}.`,
+                        });
+                    }
+                });
+            };
         }
 
-        const proxy = new Proxy(
-            clonedSchema,
-            {
-                get(target, prop, receiver) {
-                    // eslint-disable-next-line ts/no-use-before-define
-                    if (prop in customMethods) return customMethods[prop as keyof typeof customMethods];
-                    const originalProperty = Reflect.get(target, prop, receiver);
-                    if (typeof originalProperty === 'function') {
-                        return (...args: any[]) => {
-                            const result = originalProperty.apply(schema, args);
-                            if (result instanceof ZodType) return decorate(result);
-                            return result;
-                        };
-                    }
+        inst.commonImages = createCheckMimeTypesFunction(commonImageMimeTypes);
+        inst.gif = createCheckMimeTypesFunction('image/gif');
+        inst.jpeg = createCheckMimeTypesFunction('image/jpeg');
+        inst.maxSize = (bytes, params) => inst.check(_maxSize(bytes, params));
+        inst.maxSizeKb = (kb, params) => inst.check(_maxSize(kb * 1024, params));
+        inst.maxSizeMb = (mb, params) => inst.check(_maxSize(mb * 1024 * 1024, params));
+        inst.mimeType = (...mimeTypes) => createCheckMimeTypesFunction(...mimeTypes)();
+        inst.minSize = (bytes, params) => inst.check(_minSize(bytes, params));
+        inst.minSizeKb = (kb, params) => inst.check(_minSize(kb * 1024, params));
+        inst.minSizeMb = (mb, params) => inst.check(_minSize(mb * 1024 * 1024, params));
+        inst.png = createCheckMimeTypesFunction('image/png');
+        inst.webp = createCheckMimeTypesFunction('image/webp');
+    },
+);
 
-                    return originalProperty;
-                },
-            },
-        );
+export function customFile(
+    params: $ZodCustomParams | string = { error: `Input not instance of ${BufferBlob.name}.` },
+): ZodCustomFile {
+    const normalizedParams = util.normalizeParams(params);
+    normalizedParams.params = {
+        ...normalizedParams.params,
+        _allowedMimeTypes: new Set<string>(),
+    };
 
-        const customMethods = {
-            commonImages() {
-                commonImageMimeTypes.forEach((mimeType) => allowedMimeTypes.add(mimeType));
-                return proxy;
-            },
-            gif() {
-                allowedMimeTypes.add('image/gif');
-                return proxy;
-            },
-            jpeg() {
-                allowedMimeTypes.add('image/jpeg');
-                return proxy;
-            },
-            maxSize(bytes: number) {
-                maxBytes = bytes;
-                return proxy;
-            },
-            maxSizeKb(kb: number) {
-                maxBytes = kb * 1024;
-                return proxy;
-            },
-            maxSizeMb(mb: number) {
-                maxBytes = mb * 1024 * 1024;
-                return proxy;
-            },
-            mimeType(mimeType: string) {
-                allowedMimeTypes.add(mimeType.toLowerCase());
-                return proxy;
-            },
-            mimeTypes(mimeTypes: string[]) {
-                mimeTypes.forEach((mimeType) => allowedMimeTypes.add(mimeType.toLowerCase()));
-                return proxy;
-            },
-            png() {
-                allowedMimeTypes.add('image/png');
-                return proxy;
-            },
-            webp() {
-                allowedMimeTypes.add('image/webp');
-                return proxy;
-            },
-        };
+    const inst = new ZodCustomFile({
+        abort: true,
+        check: 'custom',
+        fn: (data) => data instanceof BufferBlob,
+        type: 'custom',
+        ...normalizedParams,
+    });
 
-        return proxy as ZodCustomFile;
-    }
-
-    return decorate(zodCustomFile);
+    inst._zod.bag.Class = BufferBlob;
+    return inst;
 }
