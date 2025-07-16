2fecacb61337fae45a08500986fa34a1c565622b
kiki-kanri <a470666@gmail.com>
2025-07-16 12:29:49 +0800

    feat: add `SmartDataToBaseMongooseDocType` global type

--- src/types/data/index.ts
+++ src/types/data/index.ts
@@ -2,7 +2,28 @@ import type { AdminData } from './admin';
 
 export type {} from '@kikiutils/mongoose/types/data';
 
+type EffectiveObjectIdFields<
+    T,
+    O extends keyof T,
+> = Extract<keyof T, keyof WithAdminAuditData> extends never
+    ? O
+    : Extract<keyof WithAdminAuditData | O, keyof T>;
+
 declare global {
+    type SmartDataToBaseMongooseDocType<
+        T,
+        ObjectIdFields extends Exclude<keyof T, keyof WithAdminAuditData> = never,
+        DateFields extends Exclude<keyof T, EffectiveObjectIdFields<T, ObjectIdFields>> = never,
+        CreatedAtField extends boolean = true,
+        UpdatedAtField extends boolean = true,
+    > = DataToBaseMongooseDocType<
+        T,
+        EffectiveObjectIdFields<T, ObjectIdFields>,
+        DateFields,
+        CreatedAtField,
+        UpdatedAtField
+    >;
+
     interface ApiResponseData<D extends object | undefined = undefined, E extends string | undefined = undefined> {
         data?: D;
         errorCode?: E;
