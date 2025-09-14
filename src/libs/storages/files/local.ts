6d36ae95cc6d70964c34e82445286c617911fe17
kiki-kanri <a470666@gmail.com>
2025-09-14 11:08:09 +0800

    refactor: update `LocalFileStorage` to stop using `@kikiutils/fs-extra`

--- src/libs/storages/files/local.ts
+++ src/libs/storages/files/local.ts
@@ -1,9 +1,11 @@
-import { access } from 'node:fs/promises';
-
 import { Path } from '@kikiutils/classes/path';
 import type { PathLike } from '@kikiutils/classes/path';
 import { toBuffer } from '@kikiutils/shared/buffer';
 import { checkAndGetEnvValue } from '@kikiutils/shared/env';
+import {
+    chmodSync,
+    ensureDirSync,
+} from 'fs-extra';
 
 import { FileStorageProvider } from '../../../constants/file';
 
@@ -18,30 +20,40 @@ export class LocalFileStorage extends BaseFileStorage {
     constructor(config?: LocalFileStorageConfig) {
         super();
         this.#basePath = Path.resolve(config?.basePath ?? checkAndGetEnvValue('FILE_STORAGE_LOCAL_BASE_PATH'));
-        if (!this.#basePath.mkdirpSync()) {
+        try {
+            ensureDirSync(this.#basePath.toString());
+        } catch {
             // eslint-disable-next-line style/max-len
             throw new Error(`Failed to initialize storage directory: ${this.#basePath}\nPlease check if the directory exists and has correct permissions`);
         }
 
-        if (!this.#basePath.chmodSync(0o755)) {
+        try {
+            chmodSync(this.#basePath.toString(), 0o755);
+        } catch {
             // eslint-disable-next-line style/max-len
             throw new Error(`Failed to set permissions (755) for storage directory: ${this.#basePath}\nPlease check filesystem permissions for this directory`);
         }
     }
 
-    async delete(filePath: PathLike) {
-        if (await this.#basePath.join(filePath).remove()) return this.createResult();
-        return this.createResult(new Error(`Delete file failed: ${filePath}`));
+    delete(filePath: PathLike) {
+        return this
+            .#basePath
+            .join(filePath)
+            .remove()
+            .then(() => this.createResult())
+            .catch((error) => this.createResult(new Error(`Delete file failed: ${filePath}, error: ${error}`)));
     }
 
-    async exists(filePath: PathLike) {
-        try {
-            await access(this.#basePath.join(filePath).toString());
-            return this.createResult(true);
-        } catch (error) {
-            if ((error as NodeJS.ErrnoException).code === 'ENOENT') return this.createResult(false);
-            return this.createResult(error as Error);
-        }
+    exists(filePath: PathLike) {
+        return this
+            .#basePath
+            .join(filePath)
+            .access()
+            .then(() => this.createResult(true))
+            .catch((error) => {
+                if ((error as NodeJS.ErrnoException).code === 'ENOENT') return this.createResult(false);
+                return this.createResult(error);
+            });
     }
 
     async upload(input: BinaryInput, filePath?: PathLike, extension?: string) {
@@ -49,11 +61,11 @@ export class LocalFileStorage extends BaseFileStorage {
         const hash = await this.getFileHash(buffer);
         if (!filePath) filePath = this.buildFilePathFromHash(hash, extension);
         filePath = this.#basePath.join(filePath);
-        if (!await filePath.parent.mkdirp({ mode: 0o755 })) {
+        if (!await filePath.parent.ensureDir({ mode: 0o755 }).then(() => true).catch(() => false)) {
             return this.createResult(new Error(`Failed to create directory: ${filePath.parent}`));
         }
 
-        if (!await filePath.writeFile(buffer, { mode: 0o644 })) {
+        if (!await filePath.writeFile(buffer, { mode: 0o644 }).then(() => true).catch(() => false)) {
             return this.createResult(new Error(`Failed to write file: ${filePath}`));
         }
 
