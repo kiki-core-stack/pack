2b275dbbbd23afa82f1f4f505a906db08fde0bac
kiki-kanri <a470666@gmail.com>
2025-09-15 17:14:27 +0800

    chore: update default `REDIS_URL`

--- src/storages/enhanced/redis.ts
+++ src/storages/enhanced/redis.ts
@@ -1,3 +1,5 @@
 import { createEnhancedRedisStorage } from '@kikiutils/shared/storage/enhanced/redis';
 
-export const enhancedRedisStorage = createEnhancedRedisStorage(process.env.REDIS_URL || 'redis://127.0.0.1:6379');
+export const enhancedRedisStorage = createEnhancedRedisStorage(
+    process.env.REDIS_URL || 'redis://127.0.0.1:6379?protocol=resp3',
+);
