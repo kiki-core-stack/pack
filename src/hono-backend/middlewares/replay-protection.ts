5f667b60a92d09d1fb9c205694a25b5d48892113
kiki-kanri <a470666@gmail.com>
2025-09-18 11:10:45 +0800

    feat: add `createReplayProtectionMiddleware` hono backend middleware factory

---
+++ src/hono-backend/middlewares/replay-protection.ts
@@ -0,0 +1,47 @@
+import type {
+    Context,
+    Next,
+} from 'hono';
+
+import { redisInstance } from '@/constants/redis';
+
+import { throwApiError } from '../libs/api';
+
+export function createReplayProtectionMiddleware(
+    shouldProtect?: (ctx: Context) => boolean,
+    timeoutMs: number = 5 * 1000,
+    nonceRedisKeyNamespace?: ((ctx: Context) => string) | string,
+    nonceRedisTtlBufferSeconds: number = 3,
+) {
+    return async (ctx: Context, next: Next) => {
+        if (shouldProtect && !shouldProtect(ctx)) return await next();
+
+        const timestampHeader = Number(ctx.req.header('x-timestamp'));
+        if (Number.isNaN(timestampHeader)) throwApiError(400);
+        if (Math.abs(Date.now() - timestampHeader) > timeoutMs) throwApiError(403, '請確認客戶端時間是否正確');
+
+        const nonce = ctx.req.header('x-nonce');
+        if (!nonce) throwApiError(400);
+        const nonceRedisKeyScope =
+            typeof nonceRedisKeyNamespace === 'function'
+                ? nonceRedisKeyNamespace(ctx)
+                : nonceRedisKeyNamespace;
+
+        const nonceRedisKey =
+            nonceRedisKeyScope
+                ? `replayProtectionNonce:${nonceRedisKeyScope}:${nonce}`
+                : `replayProtectionNonce:${nonce}`;
+
+        if (
+            !await redisInstance.set(
+                nonceRedisKey,
+                '',
+                'EX',
+                Math.ceil(timeoutMs / 1000) + nonceRedisTtlBufferSeconds,
+                'NX',
+            )
+        ) throwApiError(403);
+
+        return await next();
+    };
+}
