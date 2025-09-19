2de25ac07579c5c146f22ef6729f005d980a14f8
kiki-kanri <a470666@gmail.com>
2025-09-19 15:51:23 +0800

    fix: current types loading

--- references.d.ts
+++
@@ -1,2 +0,0 @@
-import type {} from '@kikiutils/mongoose/plugins/assertions';
-import type {} from '@kikiutils/mongoose/types/paginate';
--- src/types/index.ts
+++ src/types/index.ts
@@ -1,6 +1,9 @@
 import type { OmitMongooseTimestampAndOtherFields } from '@kikiutils/mongoose/types';
 import type { ZodType } from 'zod';
 
+export type * as _loadTypes1 from '@kikiutils/mongoose/plugins/assertions';
+export type * as _loadTypes2 from '@kikiutils/mongoose/types/paginate';
+
 type ZodValidatorTypeExcludeField = 'id' | keyof WithAdminAuditData;
 
 declare global {
