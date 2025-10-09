85bfc6fa2bca8bd0897c873948bcadf54077fe23
kiki-kanri <a470666@gmail.com>
2025-10-09 17:16:22 +0800

    feat: update `createEnumObject` logic in socket.io event names to append hash after keys and reduce collision risk

--- src/constants/socket.io-event-names.ts
+++ src/constants/socket.io-event-names.ts
@@ -5,8 +5,16 @@ type LowercaseUppercaseLetters<T extends string> = T extends `${infer First}${in
 
 function createEnumObject<N extends string, T extends string[]>(name: N, keys: T) {
     const enumObject: Record<string, string> = {};
-    new Set(keys).forEach((key) => enumObject[key] = `${name}.${key}`.replace(/[a-z]/g, '').toLowerCase());
-    return enumObject as { [key in T[number]]: `${LowercaseUppercaseLetters<N>}.${LowercaseUppercaseLetters<key>}` };
+    new Set(keys).forEach((key) => {
+        const fullKey = `${name}.${key}`;
+        let hash = 0;
+        for (const char of fullKey) hash = (hash * 31 + char.charCodeAt(0)) >>> 0;
+        enumObject[key] = `${fullKey.replace(/[a-z]/g, '').toLowerCase()}-${hash.toString(36).slice(0, 3)}`;
+    });
+
+    return enumObject as {
+        [key in T[number]]: `${LowercaseUppercaseLetters<N>}.${LowercaseUppercaseLetters<key>}-${string}`
+    };
 }
 
 export const ToManagementSystemBackend = /* @__PURE__ */ createEnumObject(
