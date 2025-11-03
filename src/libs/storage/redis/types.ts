3b6fdf6a70f5613278a5c3e5cd74bf17e88e76d0
kiki-kanri <a470666@gmail.com>
2025-11-03 14:43:17 +0800

    refactor: replace all Promisable type to Promise in redis storage types

--- src/libs/storage/redis/types.ts
+++ src/libs/storage/redis/types.ts
@@ -1,20 +1,19 @@
 import type { Nullable } from '@kikiutils/shared/types';
-import type { Promisable } from 'type-fest';
 
 export interface RedisLikeAdapter {
-    delete: (key: string) => Promisable<number>;
-    getBuffer: (key: string) => Promisable<Nullable<Uint8Array>>;
-    has: (key: string) => Promisable<boolean>;
-    setBuffer: (key: string, value: Uint8Array) => Promisable<'OK' | void>;
-    setBufferEx: (key: string, ttlSeconds: number, value: Uint8Array) => Promisable<'OK' | void>;
-    ttl: (key: string) => Promisable<number>;
+    delete: (key: string) => Promise<number>;
+    getBuffer: (key: string) => Promise<Nullable<Uint8Array>>;
+    has: (key: string) => Promise<boolean>;
+    setBuffer: (key: string, value: Uint8Array) => Promise<'OK' | void>;
+    setBufferEx: (key: string, ttlSeconds: number, value: Uint8Array) => Promise<'OK' | void>;
+    ttl: (key: string) => Promise<number>;
 }
 
 export interface RedisLikeStorage {
-    getItem: <T = unknown>(key: string) => Promisable<Nullable<T>>;
-    getItemTtl: (key: string) => Promisable<number>;
-    hasItem: (key: string) => Promisable<boolean>;
-    removeItem: (key: string) => Promisable<boolean>;
-    setItem: (key: string, value: any) => Promisable<boolean>;
-    setItemWithTtl: (key: string, ttlSeconds: number, value: any) => Promisable<boolean>;
+    getItem: <T = unknown>(key: string) => Promise<Nullable<T>>;
+    getItemTtl: (key: string) => Promise<number>;
+    hasItem: (key: string) => Promise<boolean>;
+    removeItem: (key: string) => Promise<boolean>;
+    setItem: (key: string, value: any) => Promise<boolean>;
+    setItemWithTtl: (key: string, ttlSeconds: number, value: any) => Promise<boolean>;
 }
