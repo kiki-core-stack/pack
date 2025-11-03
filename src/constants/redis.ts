9e12be437a9b5ba8f8bde8566efd10bc0bf226ad
kiki-kanri <a470666@gmail.com>
2025-11-03 09:38:35 +0800

    fix: resolve bun RedisClient maxRetries to large issue

--- src/constants/redis.ts
+++ src/constants/redis.ts
@@ -1,3 +1,6 @@
 import { RedisClient } from 'bun';
 
-export const redisClient = new RedisClient(process.env.REDIS_URL || 'redis://127.0.0.1:6379', { maxRetries: Infinity });
+export const redisClient = new RedisClient(
+    process.env.REDIS_URL || 'redis://127.0.0.1:6379',
+    { maxRetries: 4294967295 },
+);
