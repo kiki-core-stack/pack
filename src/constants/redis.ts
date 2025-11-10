1eb3d3d5185fcfcf72102d3e379f17a337d78d62
kiki-kanri <a470666@gmail.com>
2025-11-10 10:56:49 +0800

    feat: add `redisSubscriber` constant

--- src/constants/redis.ts
+++ src/constants/redis.ts
@@ -4,3 +4,5 @@ export const redisClient = new RedisClient(
     process.env.REDIS_URL || 'redis://127.0.0.1:6379',
     { maxRetries: 4294967295 },
 );
+
+export const redisSubscriber = await redisClient.duplicate();
