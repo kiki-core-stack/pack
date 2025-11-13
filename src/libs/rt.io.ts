f77f4643ff54b866b6999bd38f85b64c691e6482
kiki-kanri <a470666@gmail.com>
2025-11-13 11:14:52 +0800

    fix: ensure events map type is correctly passed into `createWsIoClient`

--- src/libs/rt.io.ts
+++ src/libs/rt.io.ts
@@ -1,12 +1,16 @@
 import { WsIoClient } from 'ws.io-client';
 import * as wsIoPacketMsgpackCodec from 'ws.io-client/core/packet/codecs/msgpack';
+import type {
+    DefaultEventsMap,
+    EventsMap,
+} from 'ws.io-client/types';
 import type { WsIoClientConfig } from 'ws.io-client/types/config';
 
 import { isDebugMode } from '../constants';
 
 export function createWsIoClient<
-    ToServerEvents extends Record<string, (...args: any[]) => any> = Record<string, never>,
-    ToClientEvents extends Record<string, (...args: any[]) => any> = Record<string, never>,
+    ToServerEvents extends EventsMap = DefaultEventsMap,
+    ToClientEvents extends EventsMap = DefaultEventsMap,
 >(url: string, config?: Partial<WsIoClientConfig>) {
     return new WsIoClient<ToServerEvents, ToClientEvents>(
         url,
