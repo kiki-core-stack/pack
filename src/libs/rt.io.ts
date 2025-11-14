4b05479f396ad625aa526ac61d4af6288de72258
kiki-kanri <a470666@gmail.com>
2025-11-15 01:08:24 +0800

    refactor: update ws.io-client and adjust related code
    - in `createWsIoClient`, determine msgpack usage only from node env and use await import

--- src/libs/rt.io.ts
+++ src/libs/rt.io.ts
@@ -1,21 +1,24 @@
 import { WsIoClient } from 'ws.io-client';
-import * as wsIoPacketMsgpackCodec from 'ws.io-client/core/packet/codecs/msgpack';
+import type { WsIoPacketCodec } from 'ws.io-client/core/packet/codecs';
 import type {
     DefaultEventsMap,
     EventsMap,
 } from 'ws.io-client/types';
 import type { WsIoClientConfig } from 'ws.io-client/types/config';
 
-import { isDebugMode } from '../constants';
-
-export function createWsIoClient<
+export async function createWsIoClient<
     ToServerEvents extends EventsMap = DefaultEventsMap,
     ToClientEvents extends EventsMap = DefaultEventsMap,
 >(url: string, config?: Partial<WsIoClientConfig>) {
+    let packetCodec: undefined | WsIoPacketCodec;
+    if (process.env.NODE_ENV === 'production') {
+        packetCodec = (await import('ws.io-client/core/packet/codecs/msgpack')).wsIoPacketMsgpackCodec;
+    }
+
     return new WsIoClient<ToServerEvents, ToClientEvents>(
         url,
         {
-            packetCodec: isDebugMode ? undefined : wsIoPacketMsgpackCodec,
+            packetCodec,
             reconnectDelay: 125,
             ...config,
         },
