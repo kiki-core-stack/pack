9b70898eccdabd885b43a7e4731de27ec00eb8de
kiki-kanri <a470666@gmail.com>
2026-03-28 18:38:57 +0800

    refactor: update `sharp` import method for TypeScript compatibility

--- src/utils/image.ts
+++ src/utils/image.ts
@@ -2,17 +2,12 @@ import type { Buffer } from 'node:buffer';
 
 import { toBuffer } from '@kikiutils/shared/buffer';
 import type { BinaryInput } from '@kikiutils/shared/types';
-// @ts-expect-error Ignore this error.
 import sharp from 'sharp';
 import type {
     Sharp,
-    SharpInput,
     SharpOptions,
     WebpOptions,
 } from 'sharp';
-
-declare function sharp(options?: SharpOptions): Sharp;
-declare function sharp(input?: SharpInput | SharpInput[], options?: SharpOptions): Sharp;
 
 // Constants
 const animatedMimeTypes = new Set([
